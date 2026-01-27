9dc9f902eb5bea4e1587ca511f28a3cab90fb23d
Arvi-1nd <davidarvind44@gmail.com>
2026-01-27 11:02:45 +0530

    adding to check sorted problem

--- All_Arrays/array_basic.py
+++ All_Arrays/array_basic.py
@@ -33,3 +33,25 @@ def getAlternative(arr):
     
 array = [10,20,30,40,50]
 print(getAlternative(array))
+
+
+# To check if Sorted
+def checkSorted(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] >= arr[i + 1]:
+            return False
+    return True
+
+print(checkSorted([4,5,6,7,8]))
+print(checkSorted([3,2,1,0]))
+
+#Recursive solution
+
+def recsorted(arr, idx=0):
+    if idx == len(arr)-1:
+        return True
+    if arr[idx] > arr[idx + 1]:
+        return False
+    return recsorted(arr,idx+1)
+
+print(recsorted([45,34,21,11]))
