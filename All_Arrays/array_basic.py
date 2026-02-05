2bac2df8a30ede59c9a0958d021518046a7476fd
Arvi-1nd <davidarvind44@gmail.com>
2026-02-05 21:51:43 +0530

    adding rotate array

--- All_Arrays/array_basic.py
+++ All_Arrays/array_basic.py
@@ -102,3 +102,24 @@ def reverse_arr(arr):
     return arr
 
 print(reverse_arr([4,3,2,1]))
+
+# Rotate an array
+
+def rotateArr(arr,d):
+    n = len(arr)
+    
+    for _ in range(d):
+        
+        last = arr[n-1]
+        
+        for i in range(n-1,0,-1):
+            arr[i] = arr[i-1]
+        arr[0] = last
+    
+    
+arr = [1, 2, 3, 4, 5, 6]
+d = 2
+rotateArr(arr,d)
+
+for i in range(len(arr)):
+    print(arr[i], end=" ")
